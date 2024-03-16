58fd6ccb4db62f08cec17ab5be84cc082b29eeba
ALCEMIR BELINSKI JUNIOR <alcemir.jr7@gmail.com>
2024-03-16 11:07:29 -0300

    Estrutura do projeto
    Estrutura do projeto

---
+++ src/ControleFinanceiro.Api/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Teste");
+        }
+    }
+}
